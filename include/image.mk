c22db67018adcf51cf4f772177f3f5d026197924
John Crispin <john@phrozen.org>
2019-09-10 20:49:48 +0200

    hack: qsdksquashfskit4: add patched version for qsdk
    qsdk uses the old owrt hack for the sqsh/xz comp hdr
    
    Signed-off-by: John Crispin <john@phrozen.org>

--- include/image.mk
+++ include/image.mk
@@ -226,7 +226,7 @@ $(eval $(foreach S,$(JFFS2_BLOCKSIZE),$(call Image/mkfs/jffs2/template,$(S))))
 $(eval $(foreach S,$(NAND_BLOCKSIZE),$(call Image/mkfs/jffs2-nand/template,$(S))))
 
 define Image/mkfs/squashfs
-	$(STAGING_DIR_HOST)/bin/mksquashfs4 $(call mkfs_target_dir,$(1)) $@ \
+	$(STAGING_DIR_HOST)/bin/$(if $(CONFIG_TARGET_ipq),qsdk)mksquashfs4 $(call mkfs_target_dir,$(1)) $@ \
 		-nopad -noappend -root-owned \
 		-comp $(SQUASHFSCOMP) $(SQUASHFSOPT) \
 		-processors 1
